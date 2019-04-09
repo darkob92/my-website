c333a390fbdb5000346b859e5f5c8a25855c81b9
Darko <darko.bogovic92@gmail.com>
2019-04-09 16:43:41 +0200

    added start component, installed react-router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react'
-import './sass/main.scss'
-import SideBar from './components/SideBar/SideBar'
-import Navigation from './components/Navigation/Navigation'
-import Contact from './components/MainSection/Contact'
-import About from './components/MainSection/About'
-import Journey from './components/MainSection/Journey'
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './sass/main.scss';
+import SideBar from './components/SideBar/SideBar';
+import Navigation from './components/Navigation/Navigation';
+import Start from './components/MainSection/Start';
+import Contact from './components/MainSection/Contact';
+import About from './components/MainSection/About';
+import Journey from './components/MainSection/Journey';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SideBar />
-        <Navigation />
-        <Contact />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <SideBar />
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Start} />
+            <Route path="/journey" component={Journey} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
-  }
+  };
 }
 
 export default App;
---
+++ src/components/MainSection/Start.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../sass/start.scss';
+
+const Start = () => {
+  return(
+    <div className="start-container">
+      <h3>こんにちわ (Kon'nichiwa)</h3>
+
+      <div className="intro">
+        <p>I am Darko Bogovic, frontend developer based in Croatia. I enjoy creating websites and solving problems using modern frontend technologies.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Start;
